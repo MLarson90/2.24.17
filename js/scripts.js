c601d6f2a939ab9c6afc7a0f42d868aa6f99fdc5
Max Larson <mr.m.larson90@gmail.com>
2017-02-24 11:24:59 -0800

    adds css and a win function with blinking text

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,62 @@
 //Backend
+var score=0;
 var hit =function(playerSwing){
   if(playerSwing%15 === 0){
-    console.log("ping-pong");
+    $(".ping").hide();
+    $(".pong").hide();
+    $(".pingPong").show();
+    $(".rob").hide();
+    return true;
   }else if(playerSwing%5 === 0){
-    console.log('pong');
+    $(".ping").hide();
+    $(".pong").show();
+    $(".pingPong").hide();
+    $(".rob").hide();
+    return true;
   }else if (playerSwing%3 === 0){
-    console.log("ping")
+    $(".ping").show();
+    $(".pong").hide();
+    $(".pingPong").hide();
+    $(".rob").hide();
+    return true;
   }else{
-    console.log("point team rob");
+    $(".ping").hide();
+    $(".pong").hide();
+    $(".pingPong").hide();
+    $(".rob").show();
+    return false;
   };
 };
-
-
+var talley = function(data){
+  if (data === true){
+     return score = score + 1;
+  };
+};
+ var win = function(score){
+   if(score === 10){
+   $("#top").hide();
+   $(".container").hide();
+   $("#win").show();
+  };
+ };
+/*blinker*/
+function blink(elem,times,speed){
+  if (times>0 || times<0){
+    if ($(elem).hasClass("blink"))
+        $(elem).removeClass("blink");
+    else
+        $(elem).addClass("blink")
+  }
+  clearTimeout(function(){
+    blink(elem,times,speed);
+  });
+  if(times>0 || times<0){
+    setTimeout(function(){
+      blink(elem,times,speed);
+    },speed);
+    times -=5;
+  }
+};
 //Frontend
 
 $(document).ready(function(){
@@ -19,7 +64,9 @@ $(document).ready(function(){
     event.preventDefault();
     var paddle = parseInt($("input#swing").val());
     var result = hit(paddle);
-
+    var tal = talley(result);
+    var winner = win(tal);
+    var blinker= blink("#win h1", 2 , 700);
   });
 
 });
