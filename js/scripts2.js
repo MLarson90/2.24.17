0be770551acb9f14421998217fde9fe027c24fb0
Max Larson <mr.m.larson90@gmail.com>
2017-02-24 16:44:44 -0800

    add files to gh-pages

---
+++ js/scripts2.js
@@ -0,0 +1,77 @@
+//Backend
+
+var cpuScore= [];
+var score = [];
+for (var i =0; i<=30; i++){
+var hit =function(playerSwing){
+  if(playerSwing%20 === 0){
+    $(".ping").hide();
+    $(".pong").hide();
+    $(".pingPong").show();
+    $(".rob").hide();
+    $(score).push(score++);
+  }else if(playerSwing%7 === 0){
+    $(".ping").hide();
+    $(".pong").show();
+    $(".pingPong").hide();
+    $(".rob").hide();
+    $(score).push(score++);
+  }else if (playerSwing%4 === 0){
+    $(".ping").show();
+    $(".pong").hide();
+    $(".pingPong").hide();
+    $(".rob").hide();
+    $(score).push(score++);
+  }else{
+    $(".ping").hide();
+    $(".pong").hide();
+    $(".pingPong").hide();
+    $(".rob").show();
+    $(cpuScore).push(cpuScore++);
+  };
+  if(score === 10){
+    $("#top").hide();
+    $(".container").hide();
+    $("#win").show();
+  }else if(cpuScore === 15){
+    $("#top").hide();
+    $(".container").hide();
+    $("#loser").show();
+   };
+   $("#playerS").text(score);
+   $("#cpuS").text(cpuScore);
+  };
+ };
+/*blinker*/
+function blink(elem,times,speed){
+  if (times>0 || times<0){
+    if ($(elem).hasClass("blink"))
+        $(elem).removeClass("blink");
+    else
+        $(elem).addClass("blink")
+  }
+  clearTimeout(function(){
+    blink(elem,times,speed);
+  });
+  if(times>0 || times<0){
+    setTimeout(function(){
+      blink(elem,times,speed);
+    },speed);
+    times -=5;
+  }
+};
+//Frontend
+
+$(document).ready(function(){
+  $("form.userInput").submit(function(event){
+    event.preventDefault();
+    var paddle = parseInt($("input#swing").val());
+    var result = hit(paddle);
+    // tal = talley(result);
+
+    var blinker= blink("#win h1", 2 , 700);
+  });
+  $("#clickHint").click(function(){
+    $("#help").toggle();
+  });
+});
